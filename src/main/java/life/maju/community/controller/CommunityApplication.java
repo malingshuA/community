bca5c9c595cc98590cb0953ef648f90752db1ee5
malingshuA <ll_yeguang@163.com>
2019-12-06 22:10:30 +0800

    tiojiao

--- src/main/java/life/maju/community/controller/CommunityApplication.java
+++ src/main/java/life/maju/community/controller/CommunityApplication.java
@@ -1,4 +1,4 @@
-package life.maju.community;
+package life.maju.community.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
