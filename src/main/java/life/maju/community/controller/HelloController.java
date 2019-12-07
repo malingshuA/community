1020a89a9d72861230ed93c05c22707a8d3a43a7
macunju <1548040744@qq.com>
2019-12-07 11:11:39 +0800

    可以

--- src/main/java/life/maju/community/controller/HelloController.java
+++ src/main/java/life/maju/community/controller/HelloController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("")
+//@RequestMapping("hello")
 public class HelloController {
 
     @GetMapping("/he")
     public String index(){
-        return "index";
+        return "hello";
     }
 }
