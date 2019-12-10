6c0e17fc4a144ad977038566cb4eb7f92d97c3e9
malingshuA <ll_yeguang@163.com>
2019-12-10 23:21:47 +0800

    Merge branch 'master' of https://github.com/malingshuA/community
    # Conflicts:
    #	src/main/java/life/maju/community/controller/HelloController.java

--- src/main/java/life/maju/community/controller/AuthorizeController.java
+++ src/main/java/life/maju/community/controller/AuthorizeController.java
@@ -2,6 +2,7 @@ package life.maju.community.controller;
 
 
 import life.maju.community.dto.AccesstokenDTO;
+import life.maju.community.dto.GithubUser;
 import life.maju.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
 
     @Autowired
-    //把写好的实例加载到当前
+    //把写好的实例加载到当前写好的上下文
     private GithubProvider githubProvider;
 
     @GetMapping("/callback")
@@ -24,7 +25,9 @@ public class AuthorizeController {
         accesstokenDTO.setCode(code);
         accesstokenDTO.setRedirect_uri("http://Community.zhixing.life");
         accesstokenDTO.setState(state);
-        githubProvider.getAccessToken(accesstokenDTO);
+        String accessToken = githubProvider.getAccessToken(accesstokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "hello";
     }
 }
--- src/main/java/life/maju/community/provider/GithubProvider.java
+++ src/main/java/life/maju/community/provider/GithubProvider.java
@@ -1,7 +1,10 @@
 package life.maju.community.provider;
 
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import life.maju.community.dto.AccesstokenDTO;
+import life.maju.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +14,11 @@ import java.io.IOException;
 //承载Spring boot 上下文
 public class GithubProvider {
     public String getAccessToken(AccesstokenDTO accesstokenDTO){
-        public static final MediaType JSON
-                = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accesstokenDTO));
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
@@ -24,9 +26,28 @@ public class GithubProvider {
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
             System.out.println(string);
+            //String token = string.split("&")[0].split("=")[1];
+            //System.out.println(token);
             return string;
         } catch (IOException e) {
+            //e.printStackTrace();
+        }
+        return null;
+    }
 
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user/access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            //assert response.body() != null;
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            System.out.println(string);
+            return githubUser;
+        } catch (IOException e) {
         }
         return null;
     }
